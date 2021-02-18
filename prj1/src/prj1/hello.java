df88eb1d6116a3fae2d4a35f1a6147679932953d
sysadmin <sysadmin@bcalab1-m09>
2021-02-18 12:52:33 +0530

    COMMIT 2

--- prj1/src/prj1/hello.java
+++ prj1/src/prj1/hello.java
@@ -4,6 +4,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("HAVE A NICE DAY ! ");
 	}
 
 }
